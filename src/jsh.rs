38ea64cd5dde01d349a141f640ebf5403955c006
John Naylor <jonaylor89@gmail.com>
2018-08-30 20:07:30 -0400

    Split by whitespace

--- src/jsh.rs
+++ src/jsh.rs
@@ -1,33 +1,37 @@
 
-use std::io;
+use std::io::{self, Write};
 use std::vec::Vec;
 use std::path::Path;
 use std::env::set_current_dir;
 use std::process::Command;
 
-use std::io::Write;
-
-static builtin_str: &'static [&'static str; 3] = &[
+static BUILTIN_STR: &'static [&'static str; 3] = &[
     "cd",
     "help",
     "exit",
 ];
 
-static builtin_func: &'static [fn(Vec<&str>) -> i32; 3] = &[
-    sh_cd,
-    sh_help,
-    sh_exit,
+static BUILTIN_FUNC: &'static [fn(Vec<&str>) -> i32; 3] = &[
+    jsh_cd,
+    jsh_help,
+    jsh_exit,
 ];
 
 fn num_builtins() -> usize {
-    return builtin_str.len();
+    return BUILTIN_STR.len();
 }
 
 /*
  * Shell builtin functions
  */
 
-fn sh_cd(argv: Vec<&str>) -> i32 {
+fn jsh_cd(argv: Vec<&str>) -> i32 {
+
+    if !argv[1].is_empty() {
+        println!("cd needs a directory");
+        return 1; 
+    }
+
     let path = Path::new(argv[1]);
 
     if !path.exists() {
@@ -41,21 +45,23 @@ fn sh_cd(argv: Vec<&str>) -> i32 {
     return 1;
 }
 
-fn sh_help(_argv: Vec<&str>) -> i32 {
+fn jsh_help(_argv: Vec<&str>) -> i32 {
+    println!();
     println!("John Naylor");
     println!("Shell heavily influenced by Stephen Brennan's LSH");
     println!("The following are builtin:");
 
     for i in 0..num_builtins() {
-        println!("===> {}", builtin_str[i]);
+        println!("===> {}", BUILTIN_STR[i]);
     }
 
     println!("Use the `man` command for info on other programs");
+    println!();
 
     return 1;
 }
 
-fn sh_exit(_argv: Vec<&str>) -> i32 {
+fn jsh_exit(_argv: Vec<&str>) -> i32 {
     return 0;
 }
 
@@ -71,7 +77,7 @@ fn launch(argv: Vec<&str>) -> i32 {
     if let Ok(mut child) = primed_command.spawn() {
         child.wait().expect("Command failed");
     } else {
-        println!("Command not started");
+        println!("Failed to spawn child process");
     }
 
     return 1;
@@ -84,8 +90,8 @@ fn execute(argv: Vec<&str>) -> i32 {
     }
 
     for i in 0..num_builtins() {
-        if argv[0] == builtin_str[i] {
-            return builtin_func[i](argv);
+        if argv[0] == BUILTIN_STR[i] {
+            return BUILTIN_FUNC[i](argv);
         }
     }
 
@@ -100,15 +106,20 @@ pub fn shell_loop() {
     loop {
     
         print!("[jsh]==>>> ");
-        io::stdout().flush();
+        io::stdout().flush().ok().expect("error flushing buffer");
 
         line.clear();
         io::stdin().read_line(&mut line).expect("read error");
         let commands = line.clone();
-        let argv = commands.trim().rsplit(|c| 
+        let argv = commands.trim().split_whitespace().collect();
+        /* let argv = commands.trim().rsplit(|c| 
+                            c == ' ' ||
                            c == '\t' ||
                            c == '\r' ||
                            c == '\n').collect();
+
+        */
+
         status = execute(argv);
 
         if status == 0 {
