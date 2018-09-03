25011e8f4200a2d7cfe5774987ecfa446b5dd3c5
John Naylor <jonaylor89@gmail.com>
2018-09-03 18:05:43 -0400

    Colors and style

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ extern crate termion;
 mod jsh;
 mod prompt;
 
+use termion::{color, style}
+
 fn main() {
 
     println!();
-    println!("Welcome to jsh!!!");
+    println!("{}Welcome to jsh!!!{}", style::Bold, style::Reset);
+    println!("Use the `{}help{}` for information about the shell", color::Fg(color::blue), color::Fg(color::Reset));
     println!();
 
     jsh::shell_loop();
